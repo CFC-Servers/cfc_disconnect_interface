0947987ea74fa9f56d943ca72da427bcc75c4694
Samuel Williams <33094920+samuelWilliams99@users.noreply.github.com>
2020-12-21 22:28:08 +0000

    Made game url a convar

--- lua/autorun/server/sv_disconnectinterface_init.lua
+++ lua/autorun/server/sv_disconnectinterface_init.lua
@@ -4,6 +4,7 @@ util.AddNetworkString( "CFC_DisconnectInterface_Shutdown" )
 
 CreateConVar( "cfc_disconnect_interface_status_endpoint", "https://nanny.cfcservers.org/cfc3-ping", FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
 CreateConVar( "cfc_disconnect_interface_restart_time", 180, FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
+CreateConVar( "cfc_disconnect_interface_game_url", "https://cdn.cfcservers.org/media/dinosaur/index.html", FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
 CreateConVar( "sv_mintimeout", 900, FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
 
 AddCSLuaFile( "cfc_disconnect_interface/client/cl_ponger.lua" )
