cbc7fb04c20815aea5c4b893838399d77d8f64b7
Brandon Sturgeon <brandon@brandonsturgeon.com>
2024-02-08 13:37:03 -0800

    Update dino url

--- lua/autorun/client/cl_disconnectinterface_init.lua
+++ lua/autorun/client/cl_disconnectinterface_init.lua
@@ -1,6 +1,6 @@
 CreateConVar( "cfc_disconnect_interface_status_endpoint", "https://nanny.cfcservers.org/cfc3-ping", FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
 CreateConVar( "cfc_disconnect_interface_restart_time", 180, FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
-CreateConVar( "cfc_disconnect_interface_game_url", "https://cdn.cfcservers.org/media/dinosaur/index.html", FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
+CreateConVar( "cfc_disconnect_interface_game_url", "https://loading.cfcservers.org/dino.html", FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
 local minTimeout = CreateConVar( "sv_mintimeout", 900, FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED ):GetInt()
 
 local timeout = GetConVar( "cl_timeout" )
