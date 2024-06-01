10c6c027ba129730782e56a2ccfad9cb7dcf5f25
Brandon Sturgeon <brandon@brandonsturgeon.com>
2024-06-01 08:25:12 -0700

    Update cl_disconnectinterface_init.lua

--- lua/autorun/client/cl_disconnectinterface_init.lua
+++ lua/autorun/client/cl_disconnectinterface_init.lua
@@ -1,4 +1,4 @@
-CreateConVar( "cfc_disconnect_interface_status_endpoint", "https://nanny.cfcservers.org/cfc3-ping", FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
+CreateConVar( "cfc_disconnect_interface_status_endpoint", "https://nanny.cfcservers.org/cfc3/ping", FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
 CreateConVar( "cfc_disconnect_interface_restart_time", 180, FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
 CreateConVar( "cfc_disconnect_interface_game_url", "https://loading.cfcservers.org/dino.html", FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED )
 local minTimeout = CreateConVar( "sv_mintimeout", 900, FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_PROTECTED ):GetInt()
