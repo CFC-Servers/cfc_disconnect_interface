d076422d2a4afe5247b16f963fdc0bb35b63e037
Spicy_egg 234 <177378374+spicyegg234@users.noreply.github.com>
2026-05-02 13:54:57 -0700

    fix snd_restart obliterating linux native clients

--- lua/cfc_disconnect_interface/client/cl_interface.lua
+++ lua/cfc_disconnect_interface/client/cl_interface.lua
@@ -89,7 +89,11 @@ end
 
 local function rejoin()
     dTimer.Simple( 1, function()
-        RunConsoleCommand( "snd_restart" ) -- Restarts sound engine, good practice?
+        if system.IsLinux() or system.IsOSX then
+            RunConsoleCommand( "stopsound" ) -- snd_restart crashes on linux/macos native
+        else
+            RunConsoleCommand( "snd_restart" ) -- Restarts sound engine, good practice?
+        end
         RunConsoleCommand( "retry" )
     end )
 end
